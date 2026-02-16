3b3bfcf00ecfc9fc79fa5301ec77d28cfd943959
Losseni Dia <losdiakite@gmail.com>
2026-02-16 17:48:38 +0100

    feat: add news model and categories

---
+++ src/main/java/growzapp/backend/news/model/NewsCategory.java
@@ -0,0 +1,10 @@
+// src/main/java/com/growzapp/model/NewsCategory.java
+package growzapp.backend.news.model;
+
+public enum NewsCategory {
+    PLATFORM_UPDATE, // Mises à jour plateforme
+    INVESTMENT_OPPORTUNITY, // Opportunités d'investissement
+    PERFORMANCE_REPORT, // Rapports de performance
+    EDUCATION, // Académie et éducation
+    SECURITY // Sécurité et Transparence
+}
