2ae0fe1fec786e22a356a70ef6aa444f4399d9ac
Losseni Dia <losdiakite@gmail.com>
2026-02-18 16:33:52 +0100

    create Notification model

---
+++ src/main/java/growzapp/backend/notification/model/Notification.java
@@ -0,0 +1,27 @@
+package growzapp.backend.notification.model;
+
+import java.time.LocalDateTime;
+
+import growzapp.backend.model.entite.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime date = LocalDateTime.now();
+    private boolean isRead = false;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User recipient; // L'utilisateur qui reçoit la notification
+}
