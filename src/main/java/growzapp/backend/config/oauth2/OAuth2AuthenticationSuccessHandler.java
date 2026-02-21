113017b011b1dadc5f543e3caeb48c72abf255ce
Losseni Dia <losdiakite@gmail.com>
2026-02-21 12:44:33 +0100

    create  class OAuth2AuthenticationSuccessHandler + add method generateToken in jwtService

---
+++ src/main/java/growzapp/backend/config/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,37 @@
+package growzapp.backend.config.oauth2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import growzapp.backend.config.JwtService;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException {
+
+        CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
+
+        // Génération de ton JWT habituel
+        String token = jwtService.generateToken(user.getEmail());
+
+        // Redirection vers le Frontend avec le token en paramètre URL
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/oauth2/redirect")
+                .queryParam("token", token)
+                .build().toUriString();
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
