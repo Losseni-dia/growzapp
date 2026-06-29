a3a38e3bd09ea9e0320b95ee8dc88e804080f6b4
Losseni Dia <losdiakite@gmail.com>
2026-06-29 21:30:54 +0200

    fix(contrat): correction template PDF et ExchangeRateRepository

--- src/main/java/growzapp/backend/module/exchangerate/repository/ExchangeRateRepository.java
+++ src/main/java/growzapp/backend/module/exchangerate/repository/ExchangeRateRepository.java
@@ -1,5 +1,7 @@
 package growzapp.backend.module.exchangerate.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import growzapp.backend.module.exchangerate.model.ExchangeRate;
 
 @Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, String> {
+
+    Optional<ExchangeRate> findByCurrencyCode(String currencyCode);
 }
