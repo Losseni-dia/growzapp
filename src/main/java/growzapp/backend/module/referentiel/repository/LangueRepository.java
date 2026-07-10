40843a2e804b1bca88a990c0eb21c9fe47904526
Losseni Dia <losdiakite@gmail.com>
2026-07-10 14:19:02 +0200

    feat(referentiel): ajout findByNomIgnoreCase dans LangueRepository

--- src/main/java/growzapp/backend/module/referentiel/repository/LangueRepository.java
+++ src/main/java/growzapp/backend/module/referentiel/repository/LangueRepository.java
@@ -1,9 +1,13 @@
 package growzapp.backend.module.referentiel.repository;
 
 import growzapp.backend.module.referentiel.model.Langue;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LangueRepository extends JpaRepository<Langue, Long> {
+    Optional<Langue> findByNomIgnoreCase(String nom);
 }
